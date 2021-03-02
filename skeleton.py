5215fea83bc9bba338cf020438c503e2d0626636
optobin <oliviatobin31@gmail.com>
2021-03-02 00:17:37 -0800

    Committing. Skeleton code not finished

--- skeleton.py
+++ skeleton.py
@@ -1,61 +1,79 @@
 from socket import *
 import sys
 
+# IP address and port for the web proxy server are hard coded
+HOST = '10.0.0.27' # My computer's IP address
+PORT = 65432 # Arbitrary
+
+# Set buffer size to hold 256 bytes
+BUFFER_SIZE = 2048
+
+# Initialize empty cache that maps the filename to the webpage
+cache = dict()
 
 if len(sys.argv) <= 1:
     print('Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server')
     sys.exit(2)
 
-# Create a server socket, bind it to a port and start listening
-tcpSerSock = socket(AF_INET, SOCK_STREAM)
-
-# Fill in start.
-# Fill in end.
+# Create a server socket, bind it to a port
+proxy_socket = socket(AF_INET, SOCK_STREAM)
+proxy_socket.bind(HOST, PORT)
+print("proxy socket binded to {}:{}".format(HOST, PORT))
+# Start listening
+proxy_socket.listen(5)
+print("Proxy Socket is listening")
 
 while 1:
-    # Strat receiving data from the client
+    # Start receiving data from the client
     print('Ready to serve...')
-    tcpCliSock, addr = tcpSerSock.accept()
-    print('Received a connection from:', addr)
-    message = # Fill in start. # Fill in end.
+    # Accept returns a tuple: IP address, port number
+    client_ip, client_port = proxy_socket.accept()
+    print('Received a connection from:', client_ip, client_port)
+    # Decode data received from client
+    received_data = client_ip.recv(BUFFER_SIZE)
+    message = received_data.decode('utf-8')
     print(message)
-
+    if not message.startswith("GET"):
+        # Don't know how to process anything that isn't a get request
+        response = "HTTP/1.1 501 Not Implemented"
+        response_encoded = message.encode('utf-8')
+        # Send 501 back to client
+        client_socket = socket(AF_INET, SOCK_STREAM)
+        client_socket.bind(client_ip, client_port)
+        client_socket.send(response_encoded)
+        # Close the connection
+        client_socket.close()
+        proxy_socket.close()
+    # Message is a GET Request
     # Extract the filename from the given message
     print(message.split()[1])
-
-    filename = message.split()[1].partition("/")[2]
-    print(filename)
-    fileExist = "false"
-    filetouse = "/" + filename
-
-    print(filetouse)
-
-    try:
-        # Check wether the file exist in the cache
-        f = open(filetouse[1:], "r")
-        outputdata = f.readlines()
-        fileExist = "true"
-        # ProxyServer finds a cache hit and generates a response message
-        tcpCliSock.send("HTTP/1.0 200 OK\r\n")
-        tcpCliSock.send("Content-Type:text/html\r\n")
-        # Fill in start.
-        # Fill in end.
+    file_name = message.split()[1].partition("/")[2]
+    print("File name ", file_name)
+    # Check if the file exists in the cache already
+    if file_name not in cache:
+        # File not in cache, need to create a socket on the proxy server and send the request to the web server
+        server_socket = socket(AF_INET, SOCK_STREAM)
+        host_name = file_name.replace("www.", "", 1)
+        print("host name ", host_name)
+        # Once file is retrieved, load it into cache
+    else:
+        # File found in cache, load the file and send a response back to client
+        client_ip.send("HTTP/1.0 200 OK\r\n")
+        client_ip.send("Content-Type:text/html\r\n")
         print('Read from cache')
 
+    try:
     # Error handling for file not found in cache
     except IOError:
-        if fileExist == "false":
+        if not file_exists:
             # Create a socket on the proxyserver
-            c = # Fill in start. # Fill in end.
-            hostn = filename.replace("www.","",1)
-            print(hostn)
-
+            new_socket = socket(AF_INET, SOCK_STREAM)
+            host_name = filename.replace("www.","",1)
+            print(host_name)
             try:
                 # Connect to the socket to port 80
-                # Fill in start.
-                # Fill in end.
+                new_socket.bind(HOST, 80)
                 # Create a temporary file on this socket and ask port 80 for the file requested by the client
-
                 fileobj = c.makefile('r', 0)
                 fileobj.write("GET "+"http://" + filename + " HTTP/1.0\n\n")
                 # Read the response into buffer
